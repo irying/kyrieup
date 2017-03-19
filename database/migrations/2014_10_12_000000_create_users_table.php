44ec7c517477a691e871b1dfcd43d4ea2adf2d30
irying <kyrieup@gmail.com>
2017-03-19 23:13:46 +0800

    user_table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('avatar');
+            $table->string('confirmation_token');
+            $table->smallInteger('is_active')->default(0);
+            $table->string('comments_count')->default(0);
+            $table->json('settings')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
